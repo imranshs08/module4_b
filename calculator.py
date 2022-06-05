3d50e68ff3f52e2bfa6a010db8dc33775e39fa89
imranshs08 <imranshs08@gmail.com>
2022-06-05 12:33:37 +0530

    Substraction

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 n1 = input("Enter first number\n")
 n2 = input("Enter Second number\n")
-sum = n1+n2
-print("The sum of {n1} and {n2} is {sum}" %n1 %n2 %sum)
+sum = n1-n2
+print("The substraction of {n1} and {n2} is {sum}" %n1 %n2 %sum)
+
